f21916db7ccf97f42d7c17abf26f76fe4fa1d73f
AlonLandmann <rafael.landmann@gmail.com>
2025-01-19 21:39:53 +0800

    add cell selection mechanisms

--- components/frequencies/FrequencyMatrix.js
+++ components/frequencies/FrequencyMatrix.js
@@ -1,7 +1,9 @@
 import useWindowWidth from "@/hooks/useWindowWidth";
-import { comboIndices, deckIndices, suits, values } from "@/lib/cards";
+import { comboIndices, combos, deckIndices, sameComboValues, suits, values } from "@/lib/cards";
+import { useState } from "react";
 
 export default function FrequencyMatrix({ spot, frequencies, setFrequencies }) {
+    const [selected, setSelected] = useState([]);
     const windowWidth = useWindowWidth();
     const headerWidth = 30;
     const cellWidth = Math.max(4, Math.min(10, Math.floor(((windowWidth - 80) - headerWidth - 13) / 52)));
@@ -23,12 +25,52 @@ export default function FrequencyMatrix({ spot, frequencies, setFrequencies }) {
 
         const combo = deckIndices[card1] < deckIndices[card2] ? card1 + card2 : card2 + card1;
         const index = comboIndices[combo];
+        const isSelected = selected.includes(combo);
 
         if (frequencies[index] === 0) {
-            return "bg-neutral-900";
+            return `bg-neutral-900 ${isSelected ? "border-t border-[#ff000044]" : "border-none"}`;
         }
 
-        return "bg-neutral-950";
+        return `bg-neutral-950 ${isSelected ? "border-t border-[#ff000044]" : "border-none"}`;
+    }
+
+    function handleSelect(e, v1, v2, s1, s2) {
+        const card1 = v1 + s1;
+        const card2 = v2 + s2;
+
+        if (card1 === card2) {
+            return;
+        }
+
+        if (spot.board.includes(card1) || spot.board.includes(card2)) {
+            return;
+        }
+
+        const combo = deckIndices[card1] < deckIndices[card2] ? card1 + card2 : card2 + card1;
+
+        setSelected(p => {
+            if (e.shiftKey && (e.ctrlKey || e.metaKey)) {
+                return p.filter(c => !sameComboValues(c, combo));
+            }
+
+            if (e.ctrlKey || e.metaKey) {
+                return p.concat(combos.filter(c => sameComboValues(c, combo)));;
+            }
+
+            if (e.shiftKey && e.altKey) {
+                return p.filter(c => !sameComboValues(c, combo) || c[1] !== c[3]);
+            }
+
+            if (e.altKey) {
+                return p.concat(combos.filter(c => sameComboValues(c, combo) && c[1] === c[3]));
+            }
+
+            if (selected.includes(combo)) {
+                return p.filter(c => c !== combo);
+            }
+
+            return p.concat([combo]);
+        });
     }
 
     return (
@@ -97,7 +139,9 @@ export default function FrequencyMatrix({ spot, frequencies, setFrequencies }) {
                                         style={{
                                             width: cellWidth,
                                             height: cellWidth,
+                                            borderTopWidth: cellWidth,
                                         }}
+                                        onClick={e => handleSelect(e, v1, v2, s1, s2)}
                                     >
 
                                     </div>
